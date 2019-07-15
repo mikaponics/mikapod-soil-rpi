50f2425e89f8c79c2f0d7c125dc3ed11b165bee3
bartmika <bart@mikasoftware.com>
2019-07-15 02:01:04 -0400

    BUGFIX: Fixes in script

--- scripts/instruments.sh
+++ scripts/instruments.sh
@@ -2,4 +2,4 @@
 # Script will load up the ``Pyro4`` name server to register our distributed objects.
 # This script will be run by our ``systemctl``service.
 source /home/pi/mikapod-soil-rpi/src/instrument/env/bin/activate
-python instrument_service.py
+python /home/pi/mikapod-soil-rpi/src/instrument/instrument_service.py
